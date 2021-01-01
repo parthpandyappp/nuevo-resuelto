e4186a9ebf9e92d8e28a4303ac018b5342f1f3f9
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-01-02 03:35:48 +0530

    updated admin to add forms

--- backend/resuelto/models.py
+++ backend/resuelto/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
-class resolute():
+class resolute(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20)
-    author = models.CharField(max_length=20)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     body = models.TextField()
     done = models.BooleanField()
     created = models.DateField()
@@ -14,4 +17,4 @@ class resolute():
     expires = models.DateField()
 
     def __str__(self):
-        self.title
+        return self.title
