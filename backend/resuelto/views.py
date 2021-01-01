e25311365eea249b829d30ec0c7c63e036d67939
parthpandyappp <parthpandyappp@gmail.com>
2021-01-02 00:59:07 +0530

    Added auth

--- backend/resuelto/views.py
+++ backend/resuelto/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
 def home(request):
     return render(request, "home.html", {})
 
-
-def login(request):
-    return render(request, "login.html", {})
-
-
 def signup(request):
-    return render(request, "signup.html", {})
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+
+    context = {'form': form}
+    return render(request, "registration/signup.html", context)
 
 
 def dashboard(request):
